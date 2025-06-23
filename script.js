dec31537c9d583e554d5024efef18d84ed468539
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-23 18:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function removeSelectedColor() {
+  var select = document.getElementById("colorSelect");
+  select.remove(select.selectedIndex);
+}
+ 
+// Attach function to button's onclick directly
+document.querySelector('input[type="button"]').onclick = removeSelectedColor;
